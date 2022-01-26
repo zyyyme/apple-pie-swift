6e49afa2f2e3ac18a70e467b732cd4e8e853925f
Vladislav Levchenko <levchenkoo.vladislav@gmail.com>
2022-01-26 19:03:58 +0300

    Initial Commit

---
+++ apple-pie2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apple-pie2
+//
+//  Created by Владислав Левченко on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
